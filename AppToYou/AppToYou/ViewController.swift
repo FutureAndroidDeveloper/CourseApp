71c1730ef885a1413cd7ee595799d8f35d60a6fa
Philip Bratov <p.bratau@qittiq.by>
2021-05-20 11:07:11 +0300

    develop: first commit

---
+++ AppToYou/AppToYou/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppToYou
+//
+//  Created by Philip Bratov on 20.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
